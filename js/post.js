6176738c8b1fd457b0bdce908ed633f03e78b694
MissAdelliah <siljeandrea@outlook.com>
2026-02-14 23:57:48 +0100

    save

--- js/post.js
+++ js/post.js
@@ -1,11 +1,14 @@
+// Import helper,  check if user is logged in
 import { getFromLocalStorage } from './utils.js';
 
 const BASE_API_URL = 'https://v2.api.noroff.dev';
 const NOROFF_API_KEY = '1324424e-7f11-49f7-9eb6-68a83f0cdd43';
 
-// Logged in show name, otherw demo blog
+// If not logged in, show demo blog
 const DEFAULT_BLOG_NAME = 'fitwithMalene';
+// If logged in, store name
 const profileName = getFromLocalStorage('profileName');
+//use logged in name, fallback deafault blog
 const BLOG_NAME = profileName || DEFAULT_BLOG_NAME;
 
 //DOM
@@ -22,4 +25,41 @@ const saveBtn = document.getElementById('save-btn');
 
 function showMessage(text) {
   if (messageEl) messageEl.textContent = text;
+}
+function getIdFromUrl() {
+  const params = new URLSearchParams(window.location.search);
+  return params.get('id');
+}
+function formatDate(isoString) {
+  const d = new date(isoString);
+  return d.toLocaleDateString(undefined, {
+    day: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  });
+}
+
+async function fetchPost(id) {
+  //GET /blog/post/<name>/<id>
+  const url = `${BASE_API_URL}/blog/posts/${BLOG_NAME}/${id}`;
+
+  const res = await fetch(url, {
+    headers: {
+      'X-Noroff-API-Key': NOROFF_API_KEY,
+    },
+  });
+
+  const json = await res.json();
+  return json.data;
+}
+
+//render
+function renderPost(post) {
+  titleEl.textContent = post.title || 'untitled';
+  authorEl.textContent = post.author?.name || 'unknown';
+
+  if (post.created) {
+    dateEl.textContent = formatDate(post.created);
+    dateEl.setAttribute = ('datetime', post.created);
+  }
 }
