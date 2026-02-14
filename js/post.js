e5ea219d6aea8ba6886af1f09f54f6c42b580174
MissAdelliah <siljeandrea@outlook.com>
2026-02-14 23:09:07 +0100

    save

--- js/post.js
+++ js/post.js
@@ -1,10 +1,10 @@
-import { getFromLocalStorage } from '.utils.js';
+import { getFromLocalStorage } from './utils.js';
 
 const BASE_API_URL = 'https://v2.api.noroff.dev';
 const NOROFF_API_KEY = '1324424e-7f11-49f7-9eb6-68a83f0cdd43';
 
 // Logged in show name, otherw demo blog
-const DEFAULT_BLOG_NAME = 'fitwithmalene';
+const DEFAULT_BLOG_NAME = 'fitwithMalene';
 const profileName = getFromLocalStorage('profileName');
 const BLOG_NAME = profileName || DEFAULT_BLOG_NAME;
 
